8e039d37f6a1ce2e440727a961a7ea53634a80ef
Dov Ber Ballon <dbballon+git@gmail.com>
2020-03-05 09:35:04 +0200

    build

---
+++ build/precache-manifest.35b9ea2c462b362942413805e8600d8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f2b1f0faee0952b3ef617237bc7b2a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ab3fdde46ed73517960",
+    "url": "/static/css/main.6c6ba85d.chunk.css"
+  },
+  {
+    "revision": "f27a5cbd0f4c00842ac7",
+    "url": "/static/js/2.1a2499a2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1a2499a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ab3fdde46ed73517960",
+    "url": "/static/js/main.a0a42675.chunk.js"
+  },
+  {
+    "revision": "41e4acbd4501844e4f54",
+    "url": "/static/js/runtime-main.fb3a0a13.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
